8f320619a7ec7b262214e7319483c94f9f887084
Yoshiyuki Kobayashi <frievea@gmail.com>
2022-06-04 19:25:24 +0900

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,93 @@
-# harmony_visualizer
-A tool for visualizing the harmony of MIDI input
+# Harmony Visualizer
+A tool for visualizing the harmony of MIDI input.
+
+![Harmony Visualizer](./images/harmony_visualizer.png)
+
+A tool for visualizing the harmony of the sound using virtual overtones based on MIDI input.
+
+
+<br>
+
+## How to download and install on Windows
+
+Latest version for 64bit Windows can be downloaded from the following page.
+
+https://github.com/Frieve-A/harmony_visualizer/releases
+
+Unzip the downloaded zip file and run the harmony_visualizer.exe to launch the app. No installation is required.
+
+<br>
+
+## How to execute on other platforms
+
+This application is written in Python.
+Follow the steps below to execute the Python code of the Feature Visualizer.
+
+1. git clone https://github.com/Frieve-A/harmony_visualizer.git
+2. pip install -r requirements.txt
+3. python harmony_visualizer.py
+
+<br>
+
+## How to use
+
+Connect your MIDI keyboard to your PC and launch the app.
+
+When you play a note with the MIDI keyboard, the notes you play are displayed in different colors on the keyboard for each note name. For example, Do is displayed in red, and So is displayed in blue which is a little closer to green.
+
+Above the keyboard display, a vertical line is displayed to indicate the volumme of the virtual overtones. For example, if you play Do, a vertical line indicating the second overtone is displayed at the Do position one octave higher, and the third overtone is displayed at the So position.
+
+If you input two or more sounds at the same time and the overtones are in harmony with each other, the overtones that are in harmony with the input sound are connected by a curve. The numbers on the curve indicate which overtones are in harmony.
+
+The graph on the upper left shows the volume of each overtone as a polar coordinate graph with a radius as a pitch and an angle that makes one round in one octave.
+
+Play your favorite songs and visually enjoy the harmony of the overtones!
+
+<br>
+
+---
+
+<br>
+
+MIDI入力を元にした仮想的な倍音を用いながら、音のハーモニーを可視化するツールです。
+
+
+<br>
+
+## ダウンロードとインストール（Windows）
+
+以下のページより64bit Windows用の最新バージョンをダウンロードします。
+
+https://github.com/Frieve-A/harmony_visualizer/releases
+
+ダウンロードしたzipファイルを解凍し、harmony_visualizer.exeファイル起動します。インストールは不要です。
+
+<br>
+
+## その他のプラットフォームで実行するには
+
+本アプリケーションはPythonで作成されています。
+以下の手順でFeature VisualizerのPythonコードを実行します。
+
+1. git clone https://github.com/Frieve-A/harmony_visualizer.git
+2. pip install -r requirements.txt
+3. python harmony_visualizer.py
+
+<br>
+
+## 使い方
+
+PCにMIDIキーボードを接続してアプリを起動します。
+
+MIDIキーボードで音を弾くと、弾いた音は音名毎にそれぞれ異なる色で鍵盤上に色分け表示されます。例えばドは赤色で、ソは少しグリーン寄りの青で表示されます。
+
+鍵盤表示の上には、仮想的な倍音の大きさを示す縦線が表示されます。例えばドを弾いた場合、1オクターブ上のドの位置に2倍音、ソの位置に3倍音を示す縦線が表示されます。
+
+2つ以上のを同時に入力しその倍音同士が調和している場合、入力した音と調和している倍音は曲線で結ばれます。曲線の上の数字は調和しているのが何番目の倍音であるかを示しています。
+
+左上のグラフはそれぞれの倍音の強度を、半径を音程とし、角度を1オクターブで1周するように設定した極座標グラフで表示したものです。
+
+好きな楽曲を演奏し、倍音同士が調和する様子を視覚的に楽しみましょう！
+
+
+
---
+++ harmony_visualizer.py
@@ -0,0 +1,274 @@
+import math
+import time
+import pygame
+import pygame.midi
+from pygame.locals import *
+import numpy as np
+
+app_title = 'Harmony visualizer'
+screen_size = (1920, 1080) # (3840, 2160)
+base_size = screen_size[0] // 240 # 8 pixel in 1920 x 1080
+keyboard_margin_x = base_size * 3
+key_width = (screen_size[0] - keyboard_margin_x * 2) / 52
+keyboard_top = base_size * 100
+energy_bottom = keyboard_top - keyboard_margin_x
+white_key_width = key_width * 33 / 36 #22.5 / 23.5
+white_key_height = key_width * 150 / 23.5
+black_key_width = key_width * 23 / 36 #15 / 23.5
+black_key_height = key_width * 100 / 23.5
+min_note_no = 21
+max_note_no = 109.2 # max spectrum frequency in note no
+energy_width = (key_width * 7) / 12 - 2
+polar_overtone_radius = screen_size[0] // 10
+polar_overtone_center = (polar_overtone_radius + base_size * 7, polar_overtone_radius + base_size * 19)
+tone_color = np.array([
+    (255, 128, 128),
+    (224, 160, 128),
+    (192, 192, 128),
+    (160, 224, 128),
+    (128, 255, 128),
+    (128, 224, 160),
+    (128, 192, 192),
+    (128, 160, 224),
+    (128, 128, 255),
+    (160, 128, 224),
+    (192, 128, 192),
+    (224, 128, 160)
+])
+
+def prepare_midi_ins():
+    pygame.midi.init()
+    midi_ins = []
+    midi_in_info = ''
+    for i in range(pygame.midi.get_count()):
+        midi_device_info = pygame.midi.get_device_info(i)
+        if midi_device_info[2] and not midi_device_info[4]: # is input device and not opened.
+            try:
+                midi_ins.append(pygame.midi.Input(i))
+            except:
+                # Failed to open MIDI in
+                continue
+            if midi_in_info:
+                midi_in_info += ' and '
+            midi_in_info += '"' + midi_device_info[1].decode('utf-8') + '"'
+    if midi_in_info:
+        midi_in_info = 'Listening ' + midi_in_info + ' MIDI in(s).'
+    else:
+        midi_in_info = 'No available MIDI In found.'
+    return midi_ins, midi_in_info
+
+def prepare_keyboard():
+    keys = []
+
+    class Key:
+        pass
+    for i in range(128):
+        oct = i // 12 - 1
+        note = i % 12
+        black_key = note in [1, 3, 6, 8, 10]
+        x = keyboard_margin_x + key_width * (oct * 7 + [0.5, 0.925, 1.5, 2.075, 2.5, 3.5, 3.85, 4.5, 5, 5.5, 6.15, 6.5][note] - 5)
+        key= Key()
+        key.note_no = i
+        key.black_key = black_key
+        key.x = x
+        key.normalized_x = keyboard_margin_x + 0.5 * key_width + (key_width * 7) / 12 * (i - 21)
+        key.note_on = False # Whether there is a sound including the damper pedal
+        key.key_on = False # Whether the keyboard is pressed
+        key.db = 0.0 # Virtual volume in DBs
+        key.attack = 0.0 # Whether immediately after the attack. 1.0 indicates that it is just after the attack
+        key.harmony = 0.0 # The volume of that harmony in DBs if the pitch is in harmony
+        key.display_harmony = 0.0 # Display value with fluctuations added to the volume of the harmony
+        keys.append(key)
+    return keys
+
+def note_no_to_x(note_no):
+    return keyboard_margin_x + 0.5 * key_width + (key_width * 7) / 12 * (note_no - 21) # 1 oct. lower
+
+def main():
+    # initialization
+    pygame.init()
+    screen = pygame.display.set_mode(screen_size)
+    full_screen = False
+
+    pygame.display.set_caption(app_title)
+
+    keyboard = prepare_keyboard()
+    damper = False
+    midi_ins, midi_in_info = prepare_midi_ins()
+
+    tone_names_font = pygame.font.Font(None, base_size * 5)
+    tone_names = ['C', '', 'D', '', 'E', 'F', '', 'G', '', 'A', '', 'B']
+    tone_name_sizes = [tone_names_font.size(tone_name) for tone_name in tone_names]
+    tone_names = [tone_names_font.render(tone_name, True, tone_color[i]) for i, tone_name in enumerate(tone_names)]
+
+    # main_loop
+    font = pygame.font.Font(None, base_size * 4)
+    app_title_text = font.render(app_title + '  /  Frieve 2022', True, (255,255,255))
+    midi_info_text = font.render(midi_in_info, True, (255,255,255))
+    terminated = False
+    last_time = time.perf_counter() - 10
+    harmony_radius = np.zeros(128)
+    while (not terminated):
+        current_time = time.perf_counter()
+        last_wait_ms = (current_time - last_time) * 1000
+        last_time = current_time
+
+        # midi in
+        for midi_in in midi_ins:
+            if midi_in.poll():
+                midi_events = midi_in.read(88)
+                for midi_event in midi_events:
+                    if midi_event[0][0] % 16 != 9: # ignore ch 10
+                        midi_event_type = midi_event[0][0] // 16
+
+                        if midi_event_type == 9: # note on
+                            keyboard[midi_event[0][1]].key_on = True
+                            keyboard[midi_event[0][1]].attack = 1.0
+                            keyboard[midi_event[0][1]].db = max(math.log(midi_event[0][2] / 127) / math.log(2.0) * 6.0 + 96.0, keyboard[midi_event[0][1]].db)
+                        elif midi_event_type == 8: # note off
+                            keyboard[midi_event[0][1]].key_on = False
+                        elif midi_event_type == 11:
+                            cc_no = midi_event[0][1]
+                            if cc_no == 64: # damper
+                                damper = midi_event[0][2] > 0
+                        # print(midi_event)
+
+        # update keyboard status
+        overtone_list = []
+        harmony_buf = [[] for i in range(128)]
+        harmony_list = []
+        for key in keyboard:
+            key.harmony = 0.0 # reset harmony volume
+        for key in keyboard:
+            key.note_on = key.key_on or (key.note_on and damper)
+            key.db -= last_wait_ms * (0.003 if key.note_on else 0.024)
+            if key.db < 0.0:
+                key.db = 0
+            else:
+                processed_note_no = [] 
+                for overtone_index in range(1, 384):
+                    energy = key.db - math.log(overtone_index) / math.log(2.0) * 3.0
+                    freq = key.note_no + math.log(overtone_index) / math.log(2.0) * 12
+                    if freq < min_note_no or energy < 60.0 or freq >= max_note_no:
+                        break
+                    overtone_list.append([key.note_no, freq, energy, key.attack])
+
+                    if overtone_index <= 10:
+                        overtone_note_no = int(round(freq))
+                        octave = overtone_index & (overtone_index - 1) == 0
+                        for harmony_note_no, harmony_overtone_index in harmony_buf[overtone_note_no]:
+                            harmony_octave = harmony_overtone_index & (harmony_overtone_index - 1) == 0 
+                            if key.note_no % 12 != harmony_note_no % 12 and harmony_note_no not in processed_note_no and (not octave or not harmony_octave):# ignore octave
+                                brightness = int(min(key.db, keyboard[harmony_note_no].db) - 60)
+                                keyboard[overtone_note_no].harmony = max(brightness, keyboard[overtone_note_no].harmony)
+                                if overtone_index > 1:
+                                    harmony_list.append([key.note_no, overtone_index, brightness])
+                                if harmony_overtone_index > 1:
+                                    harmony_list.append([harmony_note_no, harmony_overtone_index, brightness])
+                                processed_note_no.append(harmony_note_no)
+                        harmony_buf[overtone_note_no].append([key.note_no, overtone_index])
+
+            key.attack = max(key.attack - last_wait_ms * 0.02, 0.0)
+        overtone_list.sort(key = lambda x: x[2]) # sort with energy
+        harmony_list.sort(key = lambda x: x[2]) # sort with brightness
+
+        # fill screen
+        screen.fill((0,0,0))
+
+        # draw white keybed
+        for key in [key for key in keyboard[21:109] if not key.black_key]:
+            pygame.draw.rect(screen, (255, 255, 255) if not key.note_on else tone_color[key.note_no % 12], Rect(key.x - white_key_width / 2, keyboard_top, white_key_width, white_key_height - key.attack * base_size // 2))
+        # draw black keybed
+        for key in [key for key in keyboard[21:109] if key.black_key]:
+            pygame.draw.rect(screen, (0, 0, 0), Rect(key.x - black_key_width / 2, keyboard_top, black_key_width, black_key_height))
+            if key.note_on:
+                pygame.draw.rect(screen, tone_color[key.note_no % 12], Rect(key.x - black_key_width / 2 + 1, keyboard_top, black_key_width - 2, black_key_height - 1 - key.attack * base_size // 2))
+
+        # draw harmony volume
+        for key in keyboard[21:109]:
+            key.display_harmony = key.display_harmony * 0.05 + ((key.harmony + np.random.randn() * 6)* 0.95 if key.harmony > 0.0 else 0.0)
+            if key.display_harmony > base_size:
+                pygame.draw.circle(screen, (160, 160, 160), (key.normalized_x + 1, energy_bottom + 1), key.display_harmony * base_size / 24, 2)
+
+        # draw harmony
+        for i, (note_no, overtone_index, brightness) in enumerate(harmony_list):
+            x1 = note_no_to_x(note_no)
+            overtone_note_no = note_no + math.log(overtone_index) / math.log(2.0) * 12
+            x2 = note_no_to_x(overtone_note_no)
+            height = base_size * 4 + math.log(overtone_index) / math.log(2.0) * base_size * 20
+            color = tone_color[note_no % 12] * brightness // 36
+
+            point = []
+            resolution = int(x2 - x1) // 6
+            for i in range(resolution + 1):
+                point.append((x1 + (x2 - x1) * i / resolution, energy_bottom - math.sqrt(math.sin(i / resolution * 3.1415926)) * height))
+            pygame.draw.lines(screen, color, False, point, 1)
+
+            # draw overtone_index
+            if i >= len(harmony_list) - 16:
+                overtone_text = font.render(str(overtone_index), True, color)
+                overtone_text_size = font.size(str(overtone_index))
+                screen.blit(overtone_text, [(x1 + x2 - overtone_text_size[0]) // 2, energy_bottom - height - overtone_text_size[0] - base_size * 2])
+
+        # draw overtone (linear)
+        for note_no, freq, energy, attack in overtone_list:
+            height = (energy - 60.0) * base_size
+            x = note_no_to_x(freq)
+            pygame.draw.line(screen, tone_color[note_no % 12], (x, energy_bottom - height), (x, energy_bottom), (3 * base_size) // 8 if attack > 0 else base_size // 8)
+
+        # draw key center
+        for key in keyboard[21:109]:
+            pygame.draw.circle(screen, (255, 255, 255), (key.normalized_x + 1, energy_bottom + 1), (3 * base_size) // 8, 0)
+
+        # draw overtone (polar)
+        for i in range(6):
+            x = math.sin(i / 12 * math.pi * 2.0) * polar_overtone_radius
+            y = math.cos(i / 12 * math.pi * 2.0) * polar_overtone_radius
+            pygame.draw.line(screen, (64, 64, 64), (polar_overtone_center[0] + x, polar_overtone_center[1] - y), (polar_overtone_center[0] - x, polar_overtone_center[1] + y), base_size // 8)
+        pygame.draw.circle(screen, (96, 96, 96), polar_overtone_center, polar_overtone_radius, base_size // 8)
+        for i in range(12):
+            x = math.sin(i / 12 * math.pi * 2.0) * (polar_overtone_radius + base_size * 3)
+            y = math.cos(i / 12 * math.pi * 2.0) * (polar_overtone_radius + base_size * 3)
+            screen.blit(tone_names[i], [polar_overtone_center[0] + x - tone_name_sizes[i][0] / 2, polar_overtone_center[1] - y - tone_name_sizes[i][0] / 2])
+        for note_no, freq, energy, attack in overtone_list:
+            r = (freq - 24) / (max_note_no - 24) * polar_overtone_radius
+            angle = freq / 12
+            pos = (polar_overtone_center[0] + math.sin(angle * math.pi * 2.0) * r + 1, polar_overtone_center[1] - math.cos(angle * math.pi * 2.0) * r + 1)
+            if r > 0.0:
+                radius = ((energy / 8 - 7 + attack * 2) * base_size) // 8
+                if radius > 1:
+                    pygame.draw.circle(screen, tone_color[note_no % 12], pos, radius, 0)
+                else:
+                    screen.set_at((int(pos[0]), int(pos[1])), tone_color[note_no % 12])
+
+        # display info
+        screen.blit(app_title_text, [keyboard_margin_x, keyboard_margin_x])
+        screen.blit(midi_info_text, [keyboard_margin_x, keyboard_margin_x // 2 * 5])
+
+        # draw
+        pygame.display.update()
+
+        # handle keyboard events
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                terminated = True
+            elif event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    terminated = True
+                elif event.key == K_F11:
+                    full_screen = not full_screen
+                    if full_screen:
+                        screen = pygame.display.set_mode(screen_size, FULLSCREEN)
+                    else:
+                        screen = pygame.display.set_mode(screen_size)
+
+        pygame.time.wait(10)
+
+    for midi_in in midi_ins:
+        midi_in.close()
+    pygame.midi.quit()
+    pygame.quit()
+
+
+if __name__ == "__main__":
+    main()
